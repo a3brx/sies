2ed843dbe33a15149f5f26f66d76bb93099ae1ed
a3brx <a3brx@sapo.pt>
2019-11-21 12:33:28 +0000

    Feels like the first time

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdbool.h>
+#include <stdint.h>
 
 int main() {
     construct_chess();
     while(true){
         char move[6];
-        if(get_move(move) == RESIGN || check_mate(move))
+        uint8_t result = make_move(move);
+        if(result == STALEMATE || result == REPETITION){
+            printf("Draw\n");
             break;
-        if(valid_move(move)){
-            make_move(move);
+        }
+        if(result == RESIGN || CHECKMATE){
+            printf("%s wins!", get_winner());
+            break;
         }
     }
     destruct_chess();
