a0a6bd4459ee5c1c57ac8d64aeef52ffeec0e4aa
a3brx <a3brx@sapo.pt>
2019-11-25 22:21:59 +0000

    Life imitating art

---
+++ cmake-build-debug/CMakeFiles/sies.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sies.dir/chess/chess.c.o"
+  "CMakeFiles/sies.dir/chess/utils.c.o"
+  "CMakeFiles/sies.dir/main.c.o"
+  "sies"
+  "sies.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sies.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
